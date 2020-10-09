a8f63057db10a207e3b7e951c4afa8efa980f185
Ramiz Khan <ramizkhan1199@gmail.com>
2020-10-09 21:14:03 +0530

    Some changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var elems = document.querySelectorAll(".sidenav");
-    var instances = M.Sidenav.init(elems);
+    var elem = document.querySelector(".sidenav");
+    var instance = M.Sidenav.init(elem, options);
 });
 
-window.onload = () => {
-    console.log("Connected");
+const options = {
+    onCloseStart: function () {
+        document.querySelector("body").style.transition = "all .2s";
+        document.querySelector("body").style.paddingLeft = "0px";
+    },
+    onOpenStart: function () {
+        document.querySelector("body").style.transition = "all .2s";
+        document.querySelector("body").style.paddingLeft = "300px";
+    },
+    preventScrolling: false,
 };
+
+$(".card").hover(
+    function () {
+        $(this).removeClass("z-depth-1").addClass("z-depth-3");
+    },
+    function () {
+        $(this).removeClass("z-depth-3").addClass("z-depth-1");
+    }
+);
