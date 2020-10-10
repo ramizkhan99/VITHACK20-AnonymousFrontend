ff4cc93f2170dad53381b2cebcae662007aabf75
Ramiz Khan <ramizkhan1199@gmail.com>
2020-10-10 12:45:38 +0530

    Added login

---
+++ js/login.js
@@ -0,0 +1,22 @@
+$(".card").hover(
+    function () {
+        $(this).removeClass("z-depth-1").addClass("z-depth-3");
+    },
+    function () {
+        $(this).removeClass("z-depth-3").addClass("z-depth-1");
+    }
+);
+
+$(".card-action > button").hover(
+    function () {
+        $("#login-btn").css("display", "none").toggle("fast");
+    },
+    function () {
+        $("#login-btn").css("display", "block").toggle("fast");
+    }
+);
+
+$("#login").submit(function (e) {
+    e.preventDefault();
+    console.log("Form submitted");
+});
