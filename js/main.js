ec276f3a40ecd0c762bbd3635f29db5b2a746592
Ramiz Khan <ramizkhan1199@gmail.com>
2020-10-10 12:10:23 +0530

    Added login page

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     var elem = document.querySelector(".sidenav");
-    var instance = M.Sidenav.init(elem, options);
-
-    if (instance.isOpen) {
-        console.log("Hell yeah");
+    var instanceInit = M.Sidenav.init(elem, options);
+    if (instanceInit.isOpen) {
+        document.querySelector("#expand-sidenav").style.left +=
+            elem.offsetWidth - 30 + "px";
+        document.querySelector("#expand-sidenav > i").innerHTML =
+            "chevron_left";
     }
 });
 
 const options = {
     onCloseStart: function () {
-        document.querySelector("body").style.transition = "all .2s";
-        document.querySelector("body").style.paddingLeft = "0px";
+        document.querySelector("*").style.transition = "all .2s";
+        document.querySelector("*").style.paddingLeft = "0px";
     },
     onOpenStart: function () {
-        document.querySelector("body").style.transition = "all .2s";
-        document.querySelector("body").style.paddingLeft = "300px";
+        document.querySelector("*").style.transition = "all .2s";
+        document.querySelector("*").style.paddingLeft = "300px";
     },
     preventScrolling: false,
 };
@@ -27,3 +29,23 @@ $(".card").hover(
         $(this).removeClass("z-depth-3").addClass("z-depth-1");
     }
 );
+
+$("#expand-sidenav").on("click", function () {
+    $(".sidenav").toggleClass("sidenav-fixed");
+    const instance = M.Sidenav.init(
+        document.querySelector(".sidenav"),
+        options
+    );
+    if (instance.isOpen) {
+        document.querySelector("#expand-sidenav").style.left +=
+            document.querySelector(".sidenav").offsetWidth - 30 + "px";
+        document.querySelector("#expand-sidenav > i").innerHTML =
+            "chevron_left";
+    } else {
+        document.querySelector("#expand-sidenav").style.left -=
+            document.querySelector(".sidenav").offsetWidth - 30 + "px";
+        document.querySelector("#expand-sidenav > i").innerHTML =
+            "chevron_right";
+    }
+    instance.close();
+});
